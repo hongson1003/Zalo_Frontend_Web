ba69a9602a763c9ecd36d47f5f9749bef964cd84
NgoNhatThai <ngothai9a1@gmail.com>
2024-02-29 19:47:04 +0700

    css UI for Welcome srceen

--- src/pages/main/main.welcome.jsx
+++ src/pages/main/main.welcome.jsx
@@ -11,15 +11,59 @@ const informations = [
 
     }
 ]
-
-
+const pages = [
+    {
+        img: "/images/welcome1.PNG",
+        title: "Sử dụng tin nhắn nhanh để lưu trữ sẵn các tin nhắn thường dùng và gửi nhanh trong hội thoại bất kì"
+    },
+    {
+        img: "/images/welcome2.PNG",
+        title: "Bây giờ tin nhắn đã có thể tự động xóa sau khoảng thời gian nhất định"
+    },
+    {
+        img: "/images/welcome3.PNG",
+        title: "Nội dung tin nhắn được mã hóa trong suốt quá trình gửi và nhận"
+    },
+    {
+        img: "/images/welcome4.PNG",
+        title: "Trao đổi công việc mọi lúc mọi nơi"
+    },
+    {
+        img: "/images/welcome5.PNG",
+        title: "Giờ đây bạn có thể dễ dàng xuất dữ liệu và nhập dữ liệu của mình khi đổi máy tính mới"
+    },
+    {
+        img: "/images/welcome6.PNG",
+        title: "Kết nối và giải quyết công việc trên mọi thiết bị với dữ liệu luôn được đồng bộ"
+    },
+    {
+        img: "/images/welcome7.PNG",
+        title: "Đã có Zalo PC xử hết"
+    },
+    {
+        img: "/images/welcome8.PNG",
+        title: "Tiện lợi hơn, nhờ các công cụ chat trên máy tính"
+    },
+    {
+        img: "/images/welcome9.PNG",
+        title: "Với PC Zalo"
+    }
+]
+const Page = ({ imgLink, title }) => {
+    return (
+        <div className="page_wrapper">
+            <img src={imgLink} className= "page_img"/>
+            <p className= "page_title">{title}</p>
+        </div>
+    );
+};
 const Welcome = () => {
     var settings = {
         dots: true,
         infinite: true,
         speed: 500,
         slidesToShow: 1,
-        slidesToScroll: 3,
+        slidesToScroll: 1,
         arrows: true,
         autoplay: true,
         autoplaySpeed: 3000
@@ -36,10 +80,9 @@ const Welcome = () => {
                     Khám phá những tiện ích hỗ trợ làm việc và trò chuyện cùng người thân, bạn bè được tối ưu hoá cho máy tính của bạn.
                 </p>
                 <Slider {...settings}>
-                    <p>Hi 1</p>
-                    <p>Hi 2</p>
-                    <p>Hi 3</p>
-                    <p>Hi 4</p>
+                    {pages.map((page, index) => (
+                        <Page key={index} imgLink={page.img} title={page.title}/>
+                    ))}
                 </Slider>
             </div>
 
